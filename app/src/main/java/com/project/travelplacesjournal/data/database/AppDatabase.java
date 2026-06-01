9d46798b90b032e1e7b2c68772b7af6c66ec65bc
Angel-V-j <stu2201681028@uni-plovdiv.bg>
2026-06-01 20:34:53 +0300

    Adding database

--- app/src/main/java/com/project/travelplacesjournal/data/database/AppDatabase.java
+++ app/src/main/java/com/project/travelplacesjournal/data/database/AppDatabase.java
@@ -1,4 +1,37 @@
 package com.project.travelplacesjournal.data.database;
 
-public class AppDatabase {
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.project.travelplacesjournal.data.dao.*;
+import com.project.travelplacesjournal.data.entities.*;
+
+@Database(
+        entities = {
+                User.class,
+                Place.class,
+                PlaceImage.class,
+                Trip.class,
+                TripPlace.class,
+                Category.class,
+                Review.class
+        },
+        version = 1,
+        exportSchema = false
+)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract UserDao userDao();
+
+    public abstract PlaceDao placeDao();
+
+    public abstract PlaceImageDao placeImageDao();
+
+    public abstract TripDao tripDao();
+
+    public abstract TripPlaceDao tripPlaceDao();
+
+    public abstract CategoryDao categoryDao();
+
+    public abstract ReviewDao reviewDao();
 }
--- app/src/main/java/com/project/travelplacesjournal/data/database/DatabaseProvider.java
+++ app/src/main/java/com/project/travelplacesjournal/data/database/DatabaseProvider.java
@@ -1,4 +1,30 @@
 package com.project.travelplacesjournal.data.database;
 
+import android.content.Context;
+
+import androidx.room.Room;
+
 public class DatabaseProvider {
+
+    private static AppDatabase instance;
+
+    private DatabaseProvider() {
+    }
+
+    public static synchronized AppDatabase getDatabase(Context context) {
+
+        if (instance == null) {
+
+            instance = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            AppDatabase.class,
+                            "travel_diary_db"
+                    )
+                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries()
+                    .build();
+        }
+
+        return instance;
+    }
 }
